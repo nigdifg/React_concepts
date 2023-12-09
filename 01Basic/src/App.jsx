680154bbabf58aeffbf3648cba1dde6e49a52ae8
Anurag <2006535@kiit.ac.in>
2023-12-09 18:38:53 +0530

    react vite file setup! DONE

---
+++ 01Basic/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>This is react app basic! </h1>
+      <h2>~Anurag</h2>
+    </>
+  )
+}
+
+export default App
