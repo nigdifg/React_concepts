0102d624d99706fcfed90902f1beb9e7d29fb01d
Anurag <2006535@kiit.ac.in>
2023-12-10 21:18:59 +0530

    learning in progress

--- 03TailwindProps/src/App.jsx
+++ 03TailwindProps/src/App.jsx
@@ -1,24 +1,32 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Card from '../components/Card'
 
+import viteLogo from '/vite.svg'
+// import './App.css'
 
 function App() {
-
-  let myObj = {
-    username: "Anurag",
-    age: 21
-  }
-  let newArr = [1,2,3]
+  const [count, setCount] = useState(0)
 
   return (
     <>
-
-    <h1 className= "bg-green-400 text-black rounded-md  text-3xl font-bold underline">Chai aur React!
-    </h1>
-    <Card user="Anurag" btnText="Click me" />
-    <Card user="Chai aur React!" btnText="visit me"/>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
