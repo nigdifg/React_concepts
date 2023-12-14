8988b73b6eb1a1e5f382c9fca31e1d8b0a96394a
Anurag <2006535@kiit.ac.in>
2023-12-14 19:58:34 +0530

    stated new project - MyBlog with database as - Appwrite || also all setup done in Appwrite!

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+console.log(import.meta.env.VITE_REACT_APP_APPWRITE_URL)
+  return (
+    <>
+    <h1>Mega Blog Project!</h1>
+    </>
+  )
+}
+
+export default App
