9a09e26edc0a9a5bed55c52bf5cc5384b26099c1
Anurag <2006535@kiit.ac.in>
2023-12-22 11:10:19 +0530

    staretd making RTE component

---
+++ MegaBlog/src/components/RTE.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Editor} from '@tinymce/tinymce-react';
+
+
+export default function RTE() {
+  return (
+    <Editor
+    
+    initialValue='default value'
+    init={
+        {
+            branding: false,
+            height:500,
+            menubar: true,
+            plugins:[
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table paste code help wordcount'  
+
+            ],
+            toolbar:
+            'undo redo | formatselect | bold italic backcolor | \
+            alignleft aligncenter alignright alignjustify | \
+            bullist numlist outdent indent | removeformat | help'
+            
+        }
+    }        
+        
+    />
+  )
+}
+
