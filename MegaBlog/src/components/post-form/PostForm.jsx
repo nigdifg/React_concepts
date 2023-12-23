be558e92f0aaaa4a80bd7d28fff06edb853d750f
Anurag <2006535@kiit.ac.in>
2023-12-23 11:33:36 +0530

    1st login and signup with post is working! properly! with few warning

--- MegaBlog/src/components/post-form/PostForm.jsx
+++ MegaBlog/src/components/post-form/PostForm.jsx
@@ -41,7 +41,7 @@ export default function PostForm({ post }) {
                 const fileId = file.$id;
                 data.featuredImage = fileId;
                 const dbPost = await appwriteService.createPost({ ...data, userId: userData.$id });
-
+                // console.log(dbPost);
                 if (dbPost) {
                     navigate(`/post/${dbPost.$id}`);
                 }
