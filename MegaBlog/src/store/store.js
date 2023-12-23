5fb5457d7ffa11295b09614306310f6baa8ae88a
Anurag <2006535@kiit.ac.in>
2023-12-23 10:55:27 +0530

    store file is working fine

--- MegaBlog/src/store/store.js
+++ MegaBlog/src/store/store.js
@@ -1,7 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authSlice from './authSlice';
-// import { combineReducers } from '@reduxjs/toolkit';
-
 
 const store = configureStore({
     reducer:{
