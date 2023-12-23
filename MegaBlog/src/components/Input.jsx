2172f562f8ce9089852be7557146b19dd4ede417
Anurag <2006535@kiit.ac.in>
2023-12-23 11:22:44 +0530

    login, signup and home page... all set!

--- MegaBlog/src/components/Input.jsx
+++ MegaBlog/src/components/Input.jsx
@@ -1,27 +1,28 @@
-import React,{useId} from 'react'
+import React, {useId} from 'react'
 
-const Input = React.forwardRef(function Input({
+const Input = React.forwardRef( function Input({
     label,
     type = "text",
-    className="",
+    className = "",
     ...props
-},ref) {
+}, ref){
     const id = useId()
-
     return (
-    <div className='w-full'>
-        {label && <label
-        className='inline-block mv-1 pl-1'
-        htmlFor={id}>
-            {label}
-        </label>}
-        <input type={type}
+        <div className='w-full'>
+            {label && <label 
+            className='inline-block mb-1 pl-1' 
+            htmlFor={id}>
+                {label}
+            </label>
+            }
+            <input
+            type={type}
             className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-            ref ={ref}
+            ref={ref}
             {...props}
             id={id}
-        />
-    </div>
+            />
+        </div>
     )
 })
 
--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,33 +1,30 @@
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
-import LogOutBtn from "./Header/LogOutBtn";
+import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-import Signup from "../pages/Signup";
-import Login from "../pages/Login";
-import AddPost from "../pages/AddPost";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
-import Button from "./Button";
 import Input from "./Input";
-import Select from "./Select";
-
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogOutBtn,
+    LogoutBtn,
     RTE,
     Signup,
     Login,
+    Button,
     PostForm,
     PostCard,
-    AuthLayout,
-    Button,
+    AuthLayout, 
     Input,
     Select,
-
 }
